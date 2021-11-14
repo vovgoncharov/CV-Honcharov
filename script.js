7929340681ce4b67c3012f6f22b96fe05482fcdb
Volodymyr Honcharov <deykdog@gmail.com>
2021-11-14 06:44:59 +0200

    init: added js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-'use strict'
+"use strict";
+const getDarkTheme = () => {
+    const html = document.documentElement;
+    return html.classList.toggle("dark");
+};
+const getDarkThemeLink = () => {
+    const gmail = document.querySelectorAll(".gmail-pro");
+    gmail.forEach((el) => {
+        return el.classList.toggle("gmail");
+    });
+};
+const getDarkThemeProgressBar = () => {
+    const theme = document.querySelectorAll(".theme-pro");
+    theme.forEach((el) => {
+        return el.classList.toggle("theme");
+    });
+};
 
-let fast = document.querySelector('.above')
+let title = document.querySelectorAll(".title");
+title.forEach((el) => {
+    el.addEventListener("click", () => {
+        getDarkTheme();
+        getDarkThemeLink();
+        getDarkThemeProgressBar();
+    });
+});
